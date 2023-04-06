ee89e16ce519467469ab1e6d8cc370ce25683b1e
zavx0z <metaversebdfl@gmail.com>
2023-04-06 20:01:53 +0300

    logo clickable

--- src/layouts/elements/Logo.jsx
+++ src/layouts/elements/Logo.jsx
@@ -1,11 +1,22 @@
 import Box from "@mui/material/Box"
+import {isMobile} from "react-device-detect"
 import * as React from "react"
 import logo from "../../images/logo.png"
+import {useNavigate} from "react-router-dom"
+import routes from "../../routes/routes"
 
-export const Logo = () =>
-    <Box
-        component="img"
-        alt="logo"
-        sx={{maxHeight: 30}}
-        src={logo}
-    />
+export const Logo = () => {
+    const navigate = useNavigate()
+    return <>
+        <Box
+            onClick={() => navigate(routes.home)}
+            component="img"
+            alt="logo"
+            sx={{
+                maxHeight: isMobile ? 30 : 30,
+                cursor: 'pointer'
+            }}
+            src={logo}
+        />
+    </>
+}
