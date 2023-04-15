45d8e86195d3635b47f7c657ce2241ac32068176
zavx0z <metaversebdfl@gmail.com>
2023-04-15 17:33:12 +0300

    move event listener to pwa

--- src/features/pwa/pwaStore.js
+++ src/features/pwa/pwaStore.js
@@ -1,4 +1,12 @@
-import pwaModel from "./model"
+import pwaModel from "./pwaModel"
 
 const pwaStore = pwaModel.create({})
+
+window.addEventListener('storage', e => {
+    if (e.key === process.env.REACT_APP_ACCESS_TOKEN)
+        e.newValue ?
+            setTimeout(() => window.location.href = '/', 1000) :
+            window.location.reload()
+})
+
 export default pwaStore
