c4eb2643e33c032afb6d63109422b72c526591b8
zavx0z <metaversebdfl@gmail.com>
2023-08-23 13:35:45 +0300

    [pkg] template

---
+++ packages/template/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import 'config/styles/tailwindCSS'
+	import 'config/styles/componentCSS'
+	import 'svelte-ripple-action/ripple.css'
+</script>
+
+<svelte:head>
+	<title>template</title>
+</svelte:head>
+<slot/>
---
+++ packages/template/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1 class="text-primary-400 m-4 text-center text-4xl">Шаблон пакета</h1>
+<div class="flex h-screen w-full flex-col items-center justify-center gap-2 text-2xl">
+	<span>
+		<a class="text-primary-500 underline" href=" ">ссылка</a>
+	</span>
+</div>
