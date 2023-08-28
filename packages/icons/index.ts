8774e246d91c159e7662c1fc263fdff8b46ff0be
zavx0z <metaversebdfl@gmail.com>
2023-08-28 17:50:20 +0300

    [icons] ScreenShareCallIcon

--- packages/icons/index.ts
+++ packages/icons/index.ts
@@ -1,13 +1,14 @@
-export { default as BotikIcon } from "./src/Botik.svelte"
-export { default as BotsIcon } from "./src/Bots.svelte"
-export { default as HumansIcon } from "./src/Humans.svelte"
-export { default as GroupsIcon } from "./src/Groups.svelte"
-export { default as SettingsIcon } from "./src/Settings.svelte"
-export { default as LoginIcon } from "./src/Login.svelte"
-export { default as SidebarCloseIcon } from "./src/SidebarClose.svelte"
+export { default as BotikIcon } from './src/Botik.svelte'
+export { default as BotsIcon } from './src/Bots.svelte'
+export { default as HumansIcon } from './src/Humans.svelte'
+export { default as GroupsIcon } from './src/Groups.svelte'
+export { default as SettingsIcon } from './src/Settings.svelte'
+export { default as LoginIcon } from './src/Login.svelte'
+export { default as SidebarCloseIcon } from './src/SidebarClose.svelte'
 
-export { default as VisibleIcon } from "./src/Visible.svelte"
-export { default as NotVisibleIcon } from "./src/NotVisible.svelte"
-export { default as CloseIcon } from "./src/Close.svelte"
+export { default as VisibleIcon } from './src/Visible.svelte'
+export { default as NotVisibleIcon } from './src/NotVisible.svelte'
+export { default as CloseIcon } from './src/Close.svelte'
 
-export { default as CallIcon } from "./src/Call.svelte"
+export { default as CallIcon } from './src/Call.svelte'
+export { default as ScreenShareIcon } from './src/ScreenShare.svelte'
