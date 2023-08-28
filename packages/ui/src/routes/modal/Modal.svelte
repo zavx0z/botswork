58e4199fcaf41a10ddadbf35e4cf14e3d39886d6
zavx0z <metaversebdfl@gmail.com>
2023-08-28 17:51:02 +0300

    [ui] Modal props redirect

--- packages/ui/src/routes/modal/Modal.svelte
+++ packages/ui/src/routes/modal/Modal.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { CloseIcon } from 'icons'
-	const tabs = [{ title: 'Вход', id: 'login' }]
+	export let closeRedirect: string = '/'
 </script>
 
 <div class="bg-surface-900/40 fixed inset-0 z-50 flex flex-col items-center justify-center backdrop-blur-3xl">
 	<slot />
 	<a
-		href="/"
+		href={closeRedirect}
 		class="bg-surface-900 text-primary-500 hover:bg-surface-800 hover:text-primary-400 fixed right-2 top-2 flex h-10 w-10 items-center justify-center rounded-3xl"
 	>
 		<CloseIcon />
