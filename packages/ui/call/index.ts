6aaabcfa8d159653185c5633ad32ca278a71fed3
zavx0z <metaversebdfl@gmail.com>
2023-08-28 17:51:26 +0300

    [ui] ButtonCallScreenShare

--- packages/ui/call/index.ts
+++ packages/ui/call/index.ts
@@ -1,2 +1,3 @@
 export { default as ButtonCall } from '../src/routes/call/ButtonCall.svelte'
 export { default as ButtonCallEnd } from '../src/routes/call/ButtonCallEnd.svelte'
+export { default as ButtonCallScreenShare } from '../src/routes/call/ButtonCallScreenShare.svelte'
