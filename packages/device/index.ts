4463e71a49eab912da31c3e0feaf49e8e51cf558
zavx0z <metaversebdfl@gmail.com>
2023-08-30 13:09:02 +0300

    [device] init

---
+++ packages/device/index.ts
@@ -0,0 +1,1 @@
+export type { Device } from './src/lib/interface'
---
+++ packages/device/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import 'config/styles'
+</script>
+
+<svelte:head>
+	<title>template</title>
+</svelte:head>
+<slot/>
