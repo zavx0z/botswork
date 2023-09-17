68bfb0ff5a55af031714a412cef27e89f14c0569
zavx0z <metaversebdfl@gmail.com>
2023-09-17 18:48:35 +0300

    Remove dist/apps/io/chat and dist/apps/io/connect JavaScript files
    Both files under the io directory namely, chat and connect were removed as part of the code cleanup process. This was done because they are no longer needed in the application. It’s good practice to remove deprecated or unused code to reduce confusion and maintainability in the future. This will save time for other developers or for yourself when you revisit the code in the future. This will also make your application smaller and faster.

--- packages/auth/src/authMachine/services.ts
+++ packages/auth/src/authMachine/services.ts
@@ -1,4 +1,5 @@
-const BaseUrl = 'http://127.0.0.1:8000'
+import { PUBLIC_HOST } from '$env/static/public'
+
 const validate = async (response: Response) => {
     const data = await response.json()
     return response.ok ? data : Promise.reject({code: response.status, error: JSON.stringify(data.detail)})
@@ -9,33 +10,33 @@ type Cred = {
 }
 
 export const join = ({username, password}: Cred) =>
-    fetch(BaseUrl + '/join', {
+    fetch(PUBLIC_HOST + '/join', {
         method: "POST",
         body: JSON.stringify({username, password}),
         headers: {'Content-Type': 'application/json'}
     }).then(validate)
 
 export const login = ({username, password}: Cred) =>
-    fetch(BaseUrl + '/login', {
+    fetch(PUBLIC_HOST + '/login', {
         method: "POST",
         body: JSON.stringify({username, password}),
         headers: {'Content-Type': 'application/json'}
     }).then(validate)
 
 export const verify = (accessToken: string) =>
-    fetch(BaseUrl + '/verify', {
+    fetch(PUBLIC_HOST + '/verify', {
         method: "GET",
         headers: {'Authorization': `Bearer ${accessToken}`}
     }).then(validate)
 
 export const refresh = (refreshToken: string) =>
-    fetch(BaseUrl + '/refresh', {
+    fetch(PUBLIC_HOST + '/refresh', {
         method: "GET",
         headers: {'Authorization': `Bearer ${refreshToken}`}
     }).then(validate)
 
 export const reset = (username: string) =>
-    fetch(BaseUrl + '/reset', {
+    fetch(PUBLIC_HOST + '/reset', {
         method: "POST",
         body: JSON.stringify({username}),
         headers: {'Content-Type': 'application/json'}
