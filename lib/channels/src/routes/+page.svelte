04f056ac0999c4f81c1902ea3dd74fd6d72ea2df
zavx0z <metaversebdfl@gmail.com>
2023-09-22 17:18:27 +0300

    sveltekit library develop
    - Авто обновление билда
    - nx вернулся

---
+++ lib/channels/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import {Io} from "$lib"
+</script>
+
+<h1>Channels lib</h1>
+<p><strong>import {"{Io}"} from "@botswork/channels" :</strong> {JSON.stringify(Io)}</p>
---
+++ lib/io/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import {Io} from "@io/channels"
+</script>
+
+<h1>Io lib</h1>
+<p><strong>import {"{Io}"} from "@botswork/channels" :</strong> {JSON.stringify(Io)}</p>
