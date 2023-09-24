cde9def1cf0cc6d630d848d84dfa60ada12107e3
zavx0z <metaversebdfl@gmail.com>
2023-09-24 14:04:01 +0300

    feat(@lib/auth):

--- libs/auth/src/routes/+layout.svelte
+++ libs/auth/src/routes/+layout.svelte
@@ -2,4 +2,9 @@
     import "@ui/tailwind/component.css"
     import "@ui/tailwind/tailwind.css"
 </script>
+<svelte:head>
+    <title>
+        @lib/auth
+    </title>
+</svelte:head>
 <slot />
---
+++ libs/auth/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<h1 class="text-primary-400 m-4 text-center text-4xl">@lib/auth</h1>
+<div class="flex h-screen w-full flex-col items-center justify-center gap-2 text-2xl">
+	<a class="text-primary-500 underline" href=" ">Пакетное приложение</a>
+</div>
