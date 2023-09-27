4f645cc0259280eb49ad22aca846131df23f64c5
zavx0z <metaversebdfl@gmail.com>
2023-09-28 00:23:01 +0300

    refactor(@lib/ui): tailwind config

--- libs/ui/src/lib/index.js
+++
@@ -1,2 +0,0 @@
-export {default as tailwindConfig} from './tailwind.config.js'
-export {default as postcssConfig} from './postcss.config.js'
---
+++ libs/ui/src/routes/+layout.svelte
@@ -0,0 +1,4 @@
+<script lang='ts'>
+	import '$lib/ui.css'
+</script>
+<slot />
