605b3b0a3274e7b2cdd58a6baab3cd410d4b124b
zavx0z <metaversebdfl@gmail.com>
2023-10-01 01:37:10 +0300

    fix icons

--- libs/icons/src/routes/+page.svelte
+++ libs/icons/src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  const getIcon = (icons: {[key: string]: any}, iconName: string) => {
+    return icons[iconName as keyof typeof icons]
+  }
+</script>
+
 <h1 class="text-primary-400 m-4 text-center text-4xl">@lib/icons</h1>
 <div class="flex gap-4 px-4 flex-col">
   {#await import("$lib")}
@@ -5,7 +11,7 @@
   {:then icons}
     {#each Object.keys(icons) as iconName}
         <span class="flex gap-1 text-primary-500">
-                <svelte:component this={icons[iconName]} />
+                <svelte:component this={getIcon(icons, iconName)} />
             <span>{iconName}</span>
         </span>
     {/each}
