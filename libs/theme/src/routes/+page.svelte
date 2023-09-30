487de991d6a7fde9cfeb2ffb82dde0364ccf5adc
zavx0z <metaversebdfl@gmail.com>
2023-10-01 02:04:16 +0300

    theme stylish

--- libs/theme/src/routes/+page.svelte
+++ libs/theme/src/routes/+page.svelte
@@ -32,10 +32,10 @@
 </svelte:head>
 <div class="flex h-screen flex-col justify-between items-center w-screen gap-4">
   <h1 class="m-4 text-center text-4xl text-primary-400">@lib/theme</h1>
-  <div class="flex gap-2 w-1/2 flex-1 items-center">
+  <div class="flex gap-2 2xl:w-1/3  lg:w-1/2 w-screen px-2 flex-1 items-center">
     {#each Object.keys(colors) as colorName}
       <div class="flex flex-col w-full">
-        <h2>{colorName}</h2>
+        <h2 class="text-center capitalize font-bold border-primary-900 border-2">{colorName}</h2>
         {#each Object.keys(colors[colorName]) as colorWeight}
           <div class="h-7" style="background-color: rgb({colors[colorName][colorWeight].join(',')})">
             {colorWeight}
