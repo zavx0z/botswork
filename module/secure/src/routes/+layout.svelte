0b48107292365eff3a31484eb5175a98d982954f
zavx0z <metaversebdfl@gmail.com>
2023-10-03 21:56:43 +0300

    @lib/dev devtools

--- module/secure/src/routes/+layout.svelte
+++ module/secure/src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import "@lib/theme/app.css"
+  import { devTools } from "@lib/dev"
 </script>
+
+<div use:devTools />
 <svelte:head>
-  <title>
-    @module/secure
-  </title>
+  <title>@module/secure</title>
 </svelte:head>
 <slot />
