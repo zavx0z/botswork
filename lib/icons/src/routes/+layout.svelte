524a49b66bd4a4befcf051380ee4879ebca8c466
zavx0z <metaversebdfl@gmail.com>
2023-10-03 12:47:15 +0300

    lib/icons eruda

--- lib/icons/src/routes/+layout.svelte
+++ lib/icons/src/routes/+layout.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-    import '@lib/theme/app.css'
+  import "@lib/theme/app.css"
+  import { onMount } from "svelte"
+  let inspector: HTMLElement
+  onMount(() => {
+    let eruda: any
+    import("eruda").then((module) => {
+      eruda = module.default
+      eruda.init({
+        container: inspector,
+        tool: ["console", "elements"],
+      })
+    })
+    return () => eruda.destroy()
+  })
 </script>
+
+<div bind:this={inspector} />
 <svelte:head>
-    <title>
-        @lib/icons
-    </title>
+  <title>@lib/icons</title>
 </svelte:head>
-<slot/>
+<slot />
