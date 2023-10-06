0b5c8c07a756e10d45473de43e94df0f9bb826b7
zavx0z <metaversebdfl@gmail.com>
2023-10-06 19:17:56 +0300

    @lib/ws clear

--- lib/ws/src/routes/+layout.svelte
+++ lib/ws/src/routes/+layout.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
   import "@lib/theme/app.css"
   import { devTools } from "@lib/dev"
-  const xstateInspect = (node: HTMLIFrameElement) => {
-    import("$lib/inspector").then((module) => {
-      const { inspect } = module
-      inspect({
-        iframe: node,
-        // url: "https://stately.ai/viz/embed?zoom=1&panel=full&showOriginalLink=0&mode=viz&pan=1&controls=1&inspect",
-      })
-    })
-  }
 </script>
 
 <div use:devTools />
-<iframe class="h-96 w-screen" use:xstateInspect title="xstate" />
 <svelte:head>
   <title>@lib/ws</title>
 </svelte:head>
