cb949fbc163e7b3ec4841592be8db743f424c95d
zavx0z <metaversebdfl@gmail.com>
2023-10-19 23:09:00 +0300

    @lib/viz layout panel activity

--- lib/viz/src/routes/+layout.svelte
+++ lib/viz/src/routes/+layout.svelte
@@ -1,8 +1,34 @@
 <script lang="ts">
   import "@lib/theme/app.css"
+  import { Activity, Button } from "@lib/ui/activity"
+  import { BotikIcon } from "@lib/icons"
+  import { page } from "$app/stores"
 </script>
 
 <svelte:head>
   <title>@lib/viz</title>
 </svelte:head>
-<slot />
+<Activity>
+  <div>
+    <Button href="/" label="главная" active={$page.url.pathname === "/"}>
+      <BotikIcon />
+    </Button>
+    <Button href="/viz" label="главная" active={$page.url.pathname.includes("viz")}>
+      <img src="/favicon.png" alt="viz" data-active={$page.url.pathname.includes("viz")} class="h-8 w-8 duration-100 hover:invert-[.5] data-[active=true]:invert-[.75]" />
+    </Button>
+    <Button href="/playground" label="playground" active={$page.url.pathname.includes("playground")}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 48 48">
+        <path
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M42.8 38.73L29 17.16v-9h1.32a1.81 1.81 0 1 0 0-3.62h-12.7a1.81 1.81 0 0 0 0 3.62h1.32v9L5.2 38.73a1.71 1.71 0 0 0-.29 1a1.79 1.79 0 0 0 1.81 1.77h34.56a1.81 1.81 0 0 0 1.59-.94a1.77 1.77 0 0 0-.07-1.83Zm-31.41-9.65h25.22"
+        />
+      </svg>
+    </Button>
+  </div>
+</Activity>
+<div class="ml-12">
+  <slot />
+</div>
