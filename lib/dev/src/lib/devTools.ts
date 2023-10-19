765edce5430d85eb2924f8939d898b0a4468b0d1
zavx0z <metaversebdfl@gmail.com>
2023-10-19 19:08:08 +0300

    @lib/dev fix eruda import in devTools action

--- lib/dev/src/lib/devTools.ts
+++ lib/dev/src/lib/devTools.ts
@@ -1,21 +1,27 @@
-import eruda from "eruda"
-
 export default (node: HTMLElement) => {
-  eruda.init({
-    container: node,
-    tool: ["console", "elements", "network"],
-    useShadowDom: true,
-    autoScale: true,
-    defaults: {
-      displaySize: 50,
-      transparency: 0.8,
-      theme: "Monokai Pro",
-    },
+  let erudaDestroy: any
+  import("eruda").then((eruda: any) => {
+    
+    eruda.init({
+      container: node,
+      tool: ["console", "elements", "network"],
+      useShadowDom: true,
+      autoScale: true,
+      defaults: {
+        displaySize: 50,
+        transparency: 0.8,
+        theme: "Monokai Pro",
+      },
+    })
+    eruda.show()
+    erudaDestroy = eruda.destroy
   })
-  eruda.show()
+
   return {
     destroy() {
-      eruda.destroy()
-    }
+      try {
+        if (erudaDestroy) erudaDestroy()
+      } catch {}
+    },
   }
 }
--- lib/dev/src/routes/+layout.svelte
+++ lib/dev/src/routes/+layout.svelte
@@ -1,6 +1,2 @@
-<script lang="ts">
-  import { devTools } from "$lib"
-</script>
 
-<div use:devTools />
 <slot />
