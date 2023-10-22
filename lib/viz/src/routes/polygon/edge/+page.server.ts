c2d0536f04010420b525e87a83449e113c69402b
zavx0z <metaversebdfl@gmail.com>
2023-10-22 03:11:37 +0300

    @lib/viz ssr render code

--- lib/viz/src/routes/polygon/edge/+page.server.ts
+++ lib/viz/src/routes/polygon/edge/+page.server.ts
@@ -1,37 +1,106 @@
+import { createMachine } from "xstate"
 import type { PageServerLoad } from "./$types"
-import Prism from "prismjs"
-import "prism-svelte"
-import "prismjs/components/prism-typescript"
-
+import { Window, type IElement } from "happy-dom"
+import { toDirectedGraph, type DirectedGraphNode } from "@xstate/graph"
+import { stringify } from "javascript-stringify"
+import { img } from "./img"
+const window = new Window()
+const document = window.document
+document.write(`
+<!doctype html>
+<html>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js"></script>
+    <script src="https://unpkg.com/prismjs@1.29.0/components/prism-typescript.min.js"></script>
+    <script src="https://unpkg.com/prismjs@1.29.0/components/prism-javascript.min.js"></script>
+    <script src="https://unpkg.com/prismjs@1.29.0/plugins/line-numbers/prism-line-numbers.min.js"></script>
+  </body>
+</html>
+`)
+await window.happyDOM.whenAsyncComplete()
+function renderCode(code: string, lang: "javascript" | "typescript" = "typescript"): Promise<string> {
+  code = code.trimStart()
+  const tempPre = document.createElement("pre")
+  tempPre.className = "line-numbers"
+  const tempCode = document.createElement("code")
+  tempCode.className = `language-${lang}`
+  tempCode.textContent = code
+  tempPre.appendChild(tempCode)
+  document.body.appendChild(tempPre)
+  return new Promise<string>((resolve, reject) => {
+    try {
+      //@ts-ignore
+      window.Prism.highlightElement(tempCode, true, () => {
+        console.log("wait result")
+        const result = tempCode.getInnerHTML()
+        document.body.removeChild(tempPre)
+        resolve(result)
+      })
+    } catch {
+      reject("Не получилось)")
+    }
+  })
+}
+const machine = createMachine(
+  {
+    context: {
+      count: 0,
+    },
+    id: "machine",
+    entry: {
+      type: "inc",
+    },
+    initial: "started",
+    states: {
+      started: {
+        on: {
+          finish: {
+            target: "final",
+          },
+          increment: {
+            actions: "inc",
+          },
+        },
+      },
+      final: {
+        type: "final",
+      },
+    },
+  },
+  {
+    actions: { inc: ({ context, event }) => {} },
+    actors: {},
+    guards: {},
+    delays: {},
+  },
+)
+const directedGraph: DirectedGraphNode = toDirectedGraph(machine.definition as any)
 export const load = (async () => {
-  const code = `
-    <script>
-        let count = 0;
-    </script>
-
-    <button on:click={() => count += 1}>{count}</button>`
-  const highlighted = Prism.highlight(code, Prism.languages.svelte, "svelte")
-  const TransitionDefinition = `
-export interface TransitionDefinition<TContext, TEvent extends EventObject> extends Omit<TransitionConfig<TContext, TEvent, any>, 'actions'> {
-  target: Array<StateNode<TContext, any, TEvent>> | undefined;
-  source: StateNode<TContext, any, TEvent>;
-  actions: Array<ActionObject<TContext, TEvent>>;
-  cond?: Guard<TContext, TEvent>;
-  eventType: TEvent['type'] | NullEvent['type'] | '*';
-  toJSON: () => {};
-}`
-  const DirectedGraphEdge = `
-type DirectedGraphEdge = {
-  id: string
-  source: AnyStateNode
-  target: AnyStateNode
-  label: DirectedGraphLabel
-  transition: TransitionDefinition<any, any>
-}`
-
+  const DirectedGraphEdge = renderCode(`
+  type DirectedGraphEdge = {
+    id: string
+    source: AnyStateNode
+    target: AnyStateNode
+    label: DirectedGraphLabel
+    transition: TransitionDefinition<any, any>
+}`)
+  const TransitionDefinition = renderCode(`
+  export interface TransitionDefinition<TContext, TEvent extends EventObject> extends Omit<TransitionConfig<TContext, TEvent, any>, 'actions'> {
+    target: Array<StateNode<TContext, any, TEvent>> | undefined;
+    source: StateNode<TContext, any, TEvent>;
+    actions: Array<ActionObject<TContext, TEvent>>;
+    cond?: Guard<TContext, TEvent>;
+    eventType: TEvent['type'] | NullEvent['type'] | '*';
+    toJSON: () => {};
+}`)
   return {
-    highlighted,
-    DirectedGraphEdge: Prism.highlight(DirectedGraphEdge, Prism.languages.typescript, "typescript"),
-    TransitionDefinition: Prism.highlight(TransitionDefinition, Prism.languages.typescript, "typescript"),
+    streamed: {
+      Edge: renderCode(`const childrenFirstEdgeFirst = ${stringify(directedGraph.children[0].edges[0], null, 2)}`, "javascript"),
+      DirectedGraphEdge,
+      TransitionDefinition,
+      img: new Promise<string>((resolve) => {
+        setTimeout(() => resolve(img), 7000)
+      }),
+    },
   }
 }) satisfies PageServerLoad
--- lib/viz/src/routes/polygon/edge/+page.svelte
+++ lib/viz/src/routes/polygon/edge/+page.svelte
@@ -2,17 +2,39 @@
   import { Editor } from "@lib/editor"
   import { stringify } from "javascript-stringify"
   import { Code } from "@lib/ui/card"
-  import edgeFinish from "./edge-finish.png"
-  import "prism-themes/themes/prism-dracula.min.css"
+  import "prism-themes/themes/prism-coldark-dark.css"
+  import "./lineNum.css"
+  import PlaceholderText from "./PlaceholderText.svelte"
+  import PlaceholderImage from "./PlaceholderImage.svelte"
+
   export let data
 </script>
 
-<div class="grid h-screen grid-cols-2">
-  <div class="flex h-full flex-col items-center gap-2 p-4">
+<div class="grid h-screen grid-cols-2 grid-rows-1">
+  <div class="flex flex-col items-center gap-2 overflow-y-auto p-4">
     <h1 class="text-center text-lg">Edge - переход между 2-мя состояниями.</h1>
-    <img src={edgeFinish} alt="edgeFinish" class="rounded-xl" />
-    <Code title="DirectedGraphEdge" code={data.DirectedGraphEdge} />
-    <Code title="TransitionDefinition" code={data.TransitionDefinition} />
+    {#await data.streamed.img}
+      <PlaceholderImage />
+    {:then value}
+      <img src={value} alt="edgeFinish" class="h-96 rounded-xl" />
+    {/await}
+    {#await data.streamed.DirectedGraphEdge}
+      <PlaceholderText />
+    {:then value}
+      <Code title="DirectedGraphEdge" code={value} />
+    {/await}
+    {#await data.streamed.TransitionDefinition}
+      <PlaceholderText />
+    {:then value}
+      <Code title="TransitionDefinition" code={value} />
+    {/await}
+  </div>
+  <div class="flex flex-col items-center gap-2 overflow-y-auto p-4">
+    {#await data.streamed.Edge}
+      <PlaceholderText />
+    {:then value}
+      <Code title="Edge" code={value} />
+    {/await}
   </div>
   <!-- <Editor content={`const childrenFirstEdgeFirst = ${stringify(data.directedGraph.children[0].edges[0], null, 2)}`} foldLevel={3} hFull foldPanel minimapEnabled /> -->
 </div>
