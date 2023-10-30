3349d23d6e07341a24be36391e740d746a6f5441
zavx0z <metaversebdfl@gmail.com>
2023-10-30 16:38:57 +0300

    code-viewer web component svelte

---
+++ nodes/code-viewer/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let name = "one"
+  // только при изменении в svelte
+  $: console.log(name)
+  setTimeout(() => (name = "yo!"), 4000)
+
+  const useNode = (node: HTMLElement) => {
+    node.addEventListener("m4-update", (e) => {
+      console.log(e)
+    })
+  }
+</script>
+
+<svelte:head>
+  <script type="module" src="../dist/CodeViewer.js"></script>
+</svelte:head>
+<main>
+  <code-viewer use:useNode {name} >
+    <p>best!</p>
+    </code-viewer>
+</main>
---
+++ nodes/code-viewer/src/lib/components/CodeViewer.svelte
@@ -0,0 +1,30 @@
+<svelte:options
+  customElement={{
+    tag: "code-viewer",
+    props: {
+      hi: {
+        reflect: true,
+        type: "String",
+        attribute: "name-attr",
+      },
+    },
+  }}
+/>
+
+<script lang="ts">
+  let title: HTMLElement
+  setTimeout(() => {
+    name = "bad"
+    title.dispatchEvent(new CustomEvent("m4-update", { composed: true, detail: "success" }))
+  }, 1000)
+  export let name = "Cool"
+  $: console.log(name)
+
+  function click(e: Event) {
+    e.target?.dispatchEvent(new CustomEvent("m4-update", { composed: true, detail: "success" }))
+  }
+</script>
+
+<h1 bind:this={title} class="color-primary-500">Hello {name}!</h1>
+<button on:click={click}>click</button>
+<slot />
