29c40df05be186336592293f90b5de402b0631bf
zavx0z <metaversebdfl@gmail.com>
2023-11-11 00:47:02 +0300

    @lib/git init

---
+++ lib/git/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "@lib/theme/app.css"
+</script>
+
+<svelte:head>
+  <title>@lib/git</title>
+</svelte:head>
+<slot />
---
+++ lib/git/vite.config.ts
@@ -0,0 +1,17 @@
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
+import mkcert from "vite-plugin-mkcert"
+export default defineConfig({
+  server: {
+    https: true,
+    proxy: {},
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+  plugins: [sveltekit(), mkcert()],
+  worker: {
+    format: "es",
+  },
+})
