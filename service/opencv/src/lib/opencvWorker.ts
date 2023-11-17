12f8b79c0459a021861cd584fce2fd24ed904817
zavx0z <metaversebdfl@gmail.com>
2023-11-17 07:41:04 +0300

    @service/opencv contours

--- service/opencv/src/lib/opencvWorker.ts
+++ service/opencv/src/lib/opencvWorker.ts
@@ -28,16 +28,30 @@ try {
   addEventListener("message", async (event) => {
     switch (event.data.type) {
       case "imread":
-        console.log(event.data.param)
         const source = await fetchAsBlob(event.data.param.element)
         const inputBmp = await createImageBitmap(source)
 
         const src = cv.imread(inputBmp)
         const dst = new cv.Mat()
 
+        let dst2 = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3)
+
         cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0)
-        cv.Canny(src, dst, 50, 100, 3, false)
+        cv.Canny(src, dst, 90, 100, 3, false)
         cv.imshow(dest, dst)
+
+        cv.threshold(src, src, 120, 200, cv.THRESH_BINARY)
+
+        let contours = new cv.MatVector()
+        let hierarchy = new cv.Mat()
+        cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+        // draw contours with random Scalar
+        for (let i = 0; i < contours.size(); ++i) {
+          let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255))
+          cv.drawContours(dst2, contours, i, color, 1, cv.LINE_8, hierarchy, 100)
+        }
+        cv.imshow(dest, dst2)
+
         src.delete()
         dst.delete()
         inputBmp.close()
