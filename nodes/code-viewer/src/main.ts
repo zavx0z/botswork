09600ac7be1b7db049c10e1b29764075fe7acee8
zavx0z <metaversebdfl@gmail.com>
2023-11-25 22:51:12 +0300

    code-viewer to component

--- nodes/code-viewer/src/main.ts
+++
@@ -1,5 +0,0 @@
-import App from "./App.svelte"
-import { mount } from "svelte"
-// @ts-ignore
-const app = mount(App, { target: document.getElementById("app") })
-export default app
--- nodes/code-viewer/svelte.config.js
+++
@@ -1,6 +0,0 @@
-import preprocess from 'svelte-preprocess';
-
-export default {
-  preprocess: preprocess(),
-  compilerOptions: { customElement: true }
-}
