992e79bbae3111d15ffe4cef01c0f8d2b78e4356
zavx0z <metaversebdfl@gmail.com>
2023-11-29 09:36:33 +0300

    @lib/viz Graphics digraph => node && edges
    в разных циклах

--- lib/viz/src/lib/Graph.svelte
+++ lib/viz/src/lib/Graph.svelte
@@ -1,33 +1,42 @@
 <script lang="ts">
   import type { AnyActor, AnyStateNode } from "xstate"
-  import { getAllEdges } from "./graph/utils"
-  import { toDirectedGraph } from "./graph/directedGraph"
+  import { toDirectedGraph, type DirectedGraphNode, type DirectedGraphEdge } from "./graph/directedGraph"
   import StateNodeViz from "./StateNodeViz.svelte"
   import { getContext, onMount } from "svelte"
   import { getElkGraph } from "./graph/elk"
 
   const service: AnyActor = getContext("service")
   let digraph = toDirectedGraph(service.getSnapshot().context.machine.root)
-  let edges = getAllEdges(digraph)
-  let node = $state<AnyStateNode>()
 
+  let edges = $state<DirectedGraphEdge[]>([])
   onMount(async () => {
-    const res = await getElkGraph(digraph)
+    const egs: DirectedGraphEdge[] = []
+    const getEdgesRecursive = (dnode: DirectedGraphNode) => {
+      egs.push(...dnode.edges)
+      dnode.children.forEach(getEdgesRecursive)
+    }
+    getEdgesRecursive(digraph)
+    edges = egs
+  })
+
+
+  let node = $state<AnyStateNode>()
+  $effect(() => {
     //@ts-ignore
-    node = res.node.stateNode
+    edges && getElkGraph(digraph).then((result) => (node = result.node.stateNode))
   })
 </script>
 
 {#if node}
   <StateNodeViz stateNode={node} {edges} />
-  <!-- <svg class="pointer-events-none fixed left-0 top-0 h-screen w-screen overflow-visible">
-    {#each edges as edge, order (edge.id)}
-      <EdgeViz {edge} {order} />
-    {/each}
-  </svg> -->
-{:else}
+{:else if edges.length > 0}
   <div class="opacity-0">
     <!-- расчет -->
     <StateNodeViz stateNode={digraph.stateNode} {edges} />
   </div>
 {/if}
+<!-- <svg class="pointer-events-none fixed left-0 top-0 h-screen w-screen overflow-visible">
+    {#each edges as edge, order (edge.id)}
+      <EdgeViz {edge} {order} />
+    {/each}
+  </svg> -->
