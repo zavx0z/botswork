f91190169e2cce5d04d0c804f0709208ddaf6458
zavx0z <metaversebdfl@gmail.com>
2023-11-30 20:56:42 +0300

    @web/portal remove 3d

--- web/portal/src/routes/(app)/+layout.svelte
+++ web/portal/src/routes/(app)/+layout.svelte
@@ -1,13 +1,8 @@
 <script lang='ts'>
 	import '@lib/theme/app.css'
-
-	// import { Canvas } from '@threlte/core'
-	// import Scene from '$lib/3d/Scene.svelte'
 </script>
 
 <div class='fixed inset-0 z-0 h-[calc(100dvh)] w-screen overscroll-none'>
-	<!-- <Canvas> -->
-		<!-- <Scene /> -->
-	<!-- </Canvas> -->
+	
 </div>
 <slot />
--- web/portal/src/routes/+layout.ts
+++ web/portal/src/routes/+layout.ts
@@ -1,8 +1,8 @@
-import { AuthMachine } from "@module/secure"
-
 export let ssr = false
 export let prerender = false
+
 import { createActor } from "xstate"
+import { AuthMachine } from "@module/secure"
 
 export const load = () => {
   const auth = createActor(AuthMachine).start()
